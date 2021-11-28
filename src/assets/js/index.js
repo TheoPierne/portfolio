dbfde90f9ea9d90e3cfc94cc88d05b41bf7218ed
Théo Pierné <theo.pierne2@gmail.com>
2021-11-28 15:09:26 +0100

    Modif locomotive 28-11 n°1

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -5,6 +5,7 @@
 		getSpeed: true,
 		getDirection: true,
 		direction: 'vertical',
+		reloadOnContextChange: true,
 		scrollFromAnywhere: true
 	});
 	console.log(scroll)
@@ -29,7 +30,7 @@ const projectsData = {
 		title: "Dissociateur de doigts",
 		subtitle: "En collaboration avec l'IRR de Lay-Saint-Christophe",
 		description: "Dans le cadre du projet de deuxièmes années de BTS SNIR, en collaboration avec l'Institut Régional de Réadaptation de Lay-Saint-Christophe, j'ai dû en tant que chef de projet gérer une équipe de 2 personnes, Nicolas Motel développeur de l'interface de l'application web et Louis Thirion développeur/concepteur du prototype de gants connectés. Nous avons dû travailler à la réalisation d'une application web et d'un prototype permettant le suivi de l'évolution de la rééducation des doigts des patients de l'IRR, grâce aux différents exercices créés par les médecins dans l'application. En tant que développeur j'ai développé l'interface graphique des paramètres de l'application, ainsi que la partie backend de l'application web.",
-		tools: "<p><strong>IDE:</strong> Sublime Text 3, Arduino IDE</p><p><strong>Gestion de projet:</strong> Git, GitHub, Google Drive, Trello, Discord</p><p><strong>Langages:</strong> JavaScript, HTML5, CSS3, EJS, C/C++</p><p><strong>Frameworks:</strong> Node.js, Electron.js</p><p><strong>Librairies:</strong> Express.js, Bulma.io, Socket.io, Moment.js, HighCharts, SerialPort</p><p><strong>Suite Office:</strong> Word, PowerPoint, Excel</p>",
+		tools: "<p><strong>IDE:</strong> Sublime Text 3, Arduino IDE</p><p><strong>Gestion de projet:</strong> Git, GitHub, Google Drive, Trello, Discord</p><p><strong>Langages:</strong> JavaScript, HTML5, CSS3, EJS, C/C++</p><p><strong>Frameworks:</strong> Electron.js</p><p><strong>Librairies:</strong> Express.js, Bulma.io, Socket.io, Moment.js, HighCharts, SerialPort</p><p><strong>Suite Office:</strong> Word, PowerPoint, Excel</p>",
 		date: {
 			start: '2020-12-18',
 			end: '2021-06-16'
@@ -44,7 +45,7 @@ const projectsData = {
 		title: "Flower-Bot",
 		subtitle: "Bot Discord multifonctions développé depuis plusieurs années",
 		description: "Flower-Bot représente mon tout premier projet de développement ! Ce bot Discord intègre un système d'xp que l'on gagne par minute que l'on soit dans un salon vocal ou en train d'écrire dans un salon textuel. Des rôles peuvent ensuite être attribués automatiquement après chaque gain de niveau. Flower-Bot possède aussi d'autres fonctionnalitées très intéressantes que je te laisse découvrir en ajoutant le bot à ton serveur Discord avec <a class=\"tag is-ligth\" target=\"_blank\" href=\"https://discord.com/oauth2/authorize?client_id=476671594962354177&permissions=8&scope=applications.commands%20bot\">ce lien</a>.",
-		tools: "<p><strong>IDE:</strong> Sublime Text 3</p><p><strong>Gestion de projet:</strong> Google Drive, Discord</p><p><strong>Langages:</strong> JavaScript</p><p><strong>Frameworks:</strong> Node.js</p><p><strong>Librairies:</strong> Discord.js, Moment.js, Canvas.js, YouTube API, Spotify API, Ytdl-Core</p>",
+		tools: "<p><strong>IDE:</strong> Sublime Text 3</p><p><strong>Gestion de projet:</strong> Google Drive, Discord</p><p><strong>Langages:</strong> JavaScript</p><p><strong>Librairies:</strong> Discord.js, Moment.js, Canvas.js, YouTube API, Spotify API, Ytdl-Core</p>",
 		date: {
 			start: '2017-11-20',
 			end: 'now'
@@ -56,7 +57,7 @@ const projectsData = {
 		title: "Spot SDK Node.js",
 		subtitle: "Librairie non officielle Node.js permettant de contrôler le robot <a class=\"is-underlined\" target=\"_blank\" href=\"https://www.bostondynamics.com/spot\">Spot de Boston Dynamics</a>",
 		description: "Librairie non officielle Node.js permettant de contrôler le robot Spot de Boston Dynamics, cette librairie est encore un travail en cours de développement !",
-		tools: "<p><strong>IDE:</strong> Sublime Text 3</p><p><strong>Langages:</strong> JavaScript</p><p><strong>Frameworks:</strong> Node.js</p><p><strong>Librairies:</strong> Protobuf, Tesseract.js</p>",
+		tools: "<p><strong>IDE:</strong> Sublime Text 3</p><p><strong>Langage:</strong> JavaScript</p><p><strong>Frameworks:</strong> gRPC</p><p><strong>Librairies:</strong> Google-Protobuf, Tesseract.js</p>",
 		date: {
 			start: '2021-02-05',
 			end: 'now'
