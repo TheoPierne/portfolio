fa457740e717eddab84a31dffe7c61d9567d7c58
TheoPierne <theo.pierne2@gmail.com>
2023-03-21 08:28:22 +0100

    Modification du code js

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register(location.href + 'sw.js', {
+	navigator.serviceWorker.register('https://theopierne.fr/sw.js', {
 		scope: './'
 	});
 }
@@ -18,114 +18,6 @@ if ('serviceWorker' in navigator) {
 })();
 
 console.log("%c  Dev by Théo Pierné — https://theopierne.fr  ", "background-color: #7a7a7a; color: #ededed; font-size:10px; padding:8px 10px 6px; border-radius:4px;");
-
-const isEn = location.pathname.includes('en');
-
-console.log('isEn', isEn);
-
-fetch(`assets/translation/${isEn ? 'en.json' : 'fr.json'}`)
-.then(res => res.json())
-.then(data => {
-	i18n.translator.add(data);
-});
-
-document.querySelector('.panel-heading').innerText = i18n('project', { year: new Date().getFullYear() });
-
-const projectsData = {
-	"spot.js": {
-		logo: {},
-		title: "Spot SDK Node.js",
-		subtitle: "Librairie non officielle Node.js permettant de contrôler le robot <a class=\"is-underlined\" target=\"_blank\" href=\"https://www.bostondynamics.com/spot\">Spot de Boston Dynamics</a>",
-		description: "Librairie non officielle Node.js permettant de contrôler le robot Spot de Boston Dynamics, cette librairie est encore un travail en cours de développement !",
-		tools: "<p><strong>IDE:</strong> Sublime Text 3</p><p><strong>Langage:</strong> JavaScript</p><p><strong>Frameworks:</strong> gRPC</p><p><strong>Librairies:</strong> Google-Protobuf, gRPC-js, OpenCV4Nodejs, Tesseract.js, Lodash, Winston</p>",
-		date: {
-			start: '2021-02-05',
-			end: 'now'
-		},
-		githubLink: 'https://github.com/TheoPierne/spot-sdk-js/'
-	},
-	"spot-server.js": {
-		logo: {},
-		title: "Spot Server Node.js",
-		subtitle: "Un simple serveur pour simuler un robot Spot à des fins de tests.",
-		description: "Pour faciliter le développement du SDK du robot Spot en JavaScript, j'ai créé ce serveur qui simule les réponses aux requêtes du SDK que pourrait renvoyer le robot Spot !",
-		tools: "<p><strong>IDE:</strong> Sublime Text 3</p><p><strong>Langage:</strong> JavaScript</p><p><strong>Frameworks:</strong> gRPC</p><p><strong>Librairies:</strong> Google-Protobuf, gRPC-js OpenCV4Nodejs, Lodash, Winston</p>",
-		date: {
-			start: '2021-02-05',
-			end: 'now'
-		},
-		githubLink: 'https://github.com/TheoPierne/spot-server-js/'
-	},
-	'mythical-launcher': {
-		logo: {},
-		title: "Mythical Launcher",
-		subtitle: "Launcher customisé pour le jeu vidéo Minecraft.",
-		description: "Ce launcher à été crée pour le serveur Mythical, pour permettre aux joueurs de rejoindre le serveur avec les bon mods ainsi que la bonne verion de Minecraft.",
-		tools: "<p><strong>IDE:</strong> Sublime Text 3</p><p><strong>Langages:</strong> HTML5, CSS3, JavaScript, EJS</p><p><strong>Librairies/API:</strong> Electron, EJS, Got, Semver, UUID, Eslint, Minecraft Protocol</p>",
-		date: {
-			start: '2022-04-10',
-			end: 'now'
-		},
-		githubLink: 'https://github.com/TheoPierne/MythicalLauncher/'
-	},
-	"ddd-irr": {
-		logo: {
-			url: '/assets/img/logoMano.png', 
-			alt: 'logo projet dissociateur de doigts'
-		},
-		title: "Dissociateur de doigts",
-		subtitle: "En collaboration avec l'IRR de Lay-Saint-Christophe",
-		description: "Dans le cadre du projet de deuxièmes années de BTS SNIR, en collaboration avec l'Institut Régional de Réadaptation de Lay-Saint-Christophe, j'ai dû en tant que chef de projet gérer une équipe de 2 personnes, Nicolas Motel développeur de l'interface de l'application web et Louis Thirion développeur/concepteur du prototype de gants connectés. Nous avons dû travailler à la réalisation d'une application web et d'un prototype permettant le suivi de l'évolution de la rééducation des doigts des patients de l'IRR, grâce aux différents exercices créés par les médecins dans l'application. En tant que développeur j'ai développé l'interface graphique des paramètres de l'application, ainsi que la partie backend de l'application web.",
-		tools: "<p><strong>IDE:</strong> Sublime Text 3, Arduino IDE</p><p><strong>Gestion de projet:</strong> Git, GitHub, Google Drive, Trello, Discord</p><p><strong>Langages:</strong> JavaScript, HTML5, CSS3, EJS, C/C++</p><p><strong>Frameworks:</strong> Electron.js</p><p><strong>Librairies:</strong> Express.js, Bulma.io, Socket.io, Moment.js, HighCharts, SerialPort</p><p><strong>Suite Office:</strong> Word, PowerPoint, Excel</p>",
-		date: {
-			start: '2020-12-18',
-			end: '2021-06-16'
-		},
-		githubLink: null
-	},
-	flower: {
-		logo: {
-			url: '/assets/img/flower.png', 
-			alt: 'logo flower-bot'
-		},
-		title: "Flower-Bot",
-		subtitle: "Bot Discord multifonctions développé depuis plusieurs années",
-		description: "Flower-Bot représente mon tout premier projet de développement ! Ce bot Discord intègre un système d'xp que l'on gagne par minute que l'on soit dans un salon vocal ou en train d'écrire dans un salon textuel. Des rôles peuvent ensuite être attribués automatiquement après chaque gain de niveau. Flower-Bot possède aussi d'autres fonctionnalitées très intéressantes que je te laisse découvrir en ajoutant le bot à ton serveur Discord avec <a class=\"tag is-ligth\" target=\"_blank\" href=\"https://discord.com/oauth2/authorize?client_id=476671594962354177&permissions=8&scope=applications.commands%20bot\">ce lien</a>.",
-		tools: "<p><strong>IDE:</strong> Sublime Text 3</p><p><strong>Gestion de projet:</strong> Google Drive, Discord</p><p><strong>Langages:</strong> JavaScript</p><p><strong>Librairies:</strong> Discord.js, Moment.js, Canvas.js, YouTube API, Spotify API, Ytdl-Core</p>",
-		date: {
-			start: '2017-11-20',
-			end: 'now'
-		},
-		githubLink: null
-	},
-	acnhMsgBox: {
-		logo: {},
-		title: "Animal Crossing: New Horizons Message Box",
-		subtitle: "Librairie permettant de créer une message box ressemblant à celle dans le jeux Animal Crossing: New Horizons.",
-		description: "Cette librairie permet de créer une message box comme dans le jeux Animal Crossing: New Horizons ! <br><img src=\"https://i.imgur.com/m67XYDX.png\" alt=\"exemple librairie acnhMsgBox\">",
-		tools: "<p><strong>IDE:</strong> Sublime Text 3</p><p><strong>Langages:</strong> JavaScript</p><p><strong>Librairies/API:</strong> SVG</p>",
-		date: {
-			start: '2019-04-24',
-			end: '2019-04-26'
-		},
-		githubLink: 'https://github.com/TheoPierne/acnhMsgBox/'
-	},
-	portfolio: {
-		logo: {
-			url: '/assets/img/bitmoji_2.png',
-			alt: 'Bitmoji Théo Pierné'
-		},
-		title: "Portfolio de Théo Pierné",
-		subtitle: "<p class=\"heart-container\">Développé avec <i class=\"heart\"></i></p>",
-		description: "",
-		tools: "<p><strong>IDE:</strong> Sublime Text 3</p><p><strong>Langages:</strong> HTML5, CSS3, JavaScript</p><p><strong>Librairies/API:</strong> Bulma.io, FontAwesome, Bulma-Collapsible, PDFObject, Twemoji</p>",
-		date: {
-			start: '2021-10-02',
-			end: 'now'
-		},
-		githubLink: 'https://github.com/TheoPierne/portfolio/'
-	}
-}
 
 twemoji.parse(document.body);
 
