3658c57541d0e06bac359a126abc2f8c4a03116a
TheoPierne <theo.pierne2@gmail.com>
2023-03-21 08:25:55 +0100

    Modification des traductions

---
+++ src/assets/translation/en.js
@@ -0,0 +1,95 @@
+const projectsData = {
+	"spot.js": {
+		logo: {},
+		title: "Spot SDK Node.js",
+		subtitle: "Unofficial Node.js library to control the <a class=\"is-underlined\" target=\"_blank\" href=\"https://www.bostondynamics.com/spot\">Boston Dynamics Spot Robot</a>",
+		description: "Unofficial Node.js library to control the Boston Dynamics Spot robot, this library is still a work in progress!",
+		tools: "<p><strong>IDE:</strong> Sublime Text 3</p><p><strong>Language:</strong> JavaScript</p><p><strong>Frameworks:</strong> gRPC</p><p><strong>Libraries:</strong> Google-Protobuf, gRPC-js, OpenCV4Nodejs, Tesseract.js, Lodash, Winston</p>",
+		date: {
+			start: '2021-02-05',
+			end: 'now'
+		},
+		githubLink: 'https://github.com/TheoPierne/spot-sdk-js/'
+	},
+	"spot-server.js": {
+		logo: {},
+		title: "Spot Server Node.js",
+		subtitle: "A simple server to simulate a Spot robot for testing purposes.",
+		description: "To facilitate the development of the Spot robot SDK in JavaScript, I created this server which simulates the responses to SDK requests that the Spot robot could return!",
+		tools: "<p><strong>IDE:</strong> Sublime Text 3</p><p><strong>Language:</strong> JavaScript</p><p><strong>Frameworks:</strong> gRPC</p><p><strong>Libraries:</strong> Google-Protobuf, gRPC-js OpenCV4Nodejs, Lodash, Winston</p>",
+		date: {
+			start: '2021-02-05',
+			end: 'now'
+		},
+		githubLink: 'https://github.com/TheoPierne/spot-server-js/'
+	},
+	'mythical-launcher': {
+		logo: {},
+		title: "Mythical Launcher",
+		subtitle: "Customized launcher for Minecraft video game.",
+		description: "This launcher was created for the Mythical server, to allow players to join the server with the correct mods and the correct version of Minecraft.",
+		tools: "<p><strong>IDE:</strong> Sublime Text 3</p><p><strong>Languages:</strong> HTML5, CSS3, JavaScript, EJS</p><p><strong>Libraries/API:</strong> Electron, EJS, Got, Semver, UUID, Eslint, Minecraft Protocol</p>",
+		date: {
+			start: '2022-04-10',
+			end: 'now'
+		},
+		githubLink: 'https://github.com/TheoPierne/MythicalLauncher/'
+	},
+	"ddd-irr": {
+		logo: {
+			url: '/assets/img/logoMano.png', 
+			alt: 'logo projet dissociateur de doigts'
+		},
+		title: "Finger dissociator",
+		subtitle: "In collaboration with the IRR of Lay-Saint-Christophe",
+		description: "<p>As part of the second year project of my 2 years degree in computer science, in collaboration with the Regional Institute of Rehabilitation of Lay-Saint-Christophe, I had as a project manager to manage a team of 2 people, Nicolas Motel developer of the interface of the web application and Louis Thirion developer/designer of the prototype of connected gloves. We had to work on the realization of a web application and a prototype allowing the follow-up of the evolution of the rehabilitation of the fingers of the patients of the IRR, thanks to the various exercises created by the doctors in the application. As a developer I developed the application settings GUI, as well as the backend part of the web application.</p>",
+		tools: "<p><strong>IDE:</strong> Sublime Text 3, Arduino IDE</p><p><strong>Project management:</strong> Git, GitHub, Google Drive, Trello, Discord</p><p><strong>Languages:</strong> JavaScript, HTML5, CSS3, EJS, C/C++</p><p><strong>Frameworks:</strong> Electron.js</p><p><strong>Libraries:</strong> Express.js, Bulma.io, Socket.io, Moment.js, HighCharts, SerialPort</p><p><strong>Suite Office:</strong> Word, PowerPoint, Excel</p>",
+		date: {
+			start: '2020-12-18',
+			end: '2021-06-16'
+		},
+		githubLink: null
+	},
+	flower: {
+		logo: {
+			url: '/assets/img/flower.png', 
+			alt: 'logo flower-bot'
+		},
+		title: "Flower-Bot",
+		subtitle: "Multifunctional Discord bot developed for several years",
+		description: "Flower-Bot represents my very first development project! This Discord bot integrates an xp system that you earn per minute whether you are in a voice room or writing in a text room. Roles can then be assigned automatically after each level up. Flower-Bot also has other very interesting features that I let you discover by adding the bot to your Discord server with <a class=\"tag is-ligth\" target=\"_blank\" href=\"https://discord.com/oauth2/authorize?client_id=476671594962354177&permissions=8&scope=applications.commands%20bot\">this link</a>.",
+		tools: "<p><strong>IDE:</strong> Sublime Text 3</p><p><strong>Project management:</strong> Google Drive, Discord</p><p><strong>Languages:</strong> JavaScript</p><p><strong>Libraries:</strong> Discord.js, Moment.js, Canvas.js, YouTube API, Spotify API, Ytdl-Core</p>",
+		date: {
+			start: '2017-11-20',
+			end: 'now'
+		},
+		githubLink: null
+	},
+	acnhMsgBox: {
+		logo: {},
+		title: "Animal Crossing: New Horizons Message Box",
+		subtitle: "Library allowing to create a message box similar to the one in the Animal Crossing: New Horizons game.",
+		description: "This library allows you to create a message box like in the Animal Crossing: New Horizons game! <br><img src=\"https://i.imgur.com/m67XYDX.png\" alt=\"example acnhMsgBox library\">",
+		tools: "<p><strong>IDE:</strong> Sublime Text 3</p><p><strong>Languages:</strong> JavaScript</p><p><strong>Librarie/API:</strong> SVG</p>",
+		date: {
+			start: '2019-04-24',
+			end: '2019-04-26'
+		},
+		githubLink: 'https://github.com/TheoPierne/acnhMsgBox/'
+	},
+	portfolio: {
+		logo: {
+			url: '/assets/img/bitmoji_2.png',
+			alt: 'Bitmoji Théo Pierné'
+		},
+		title: "Portfolio of Théo Pierné",
+		subtitle: "<p class=\"heart-container\">Developped with<i class=\"heart\"></i></p>",
+		description: "",
+		tools: "<p><strong>IDE:</strong> Sublime Text 3</p><p><strong>Languages:</strong> HTML5, CSS3, JavaScript</p><p><strong>Libraries/API:</strong> Bulma.io, FontAwesome, Bulma-Collapsible, PDFObject, Twemoji</p>",
+		date: {
+			start: '2021-10-02',
+			end: 'now'
+		},
+		githubLink: 'https://github.com/TheoPierne/portfolio/'
+	}
+}
