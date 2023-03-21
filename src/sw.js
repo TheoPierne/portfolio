030542917cb5c24eb5a5accf90c84ed271c4e03e
TheoPierne <theo.pierne2@gmail.com>
2023-03-21 08:29:57 +0100

    Maj du sw

--- src/sw.js
+++ src/sw.js
@@ -1,37 +1,39 @@
 const addToCache = [
-'/assets/webfonts/fa-brands-400.woff2',
-'/assets/webfonts/fa-solid-900.woff2',
-'/assets/doc/loritz_plaquette_snir.pdf',
-'/assets/doc/loritz_plaquette_sin.pdf',
-'/assets/img/back.png',
-'/assets/img/heart.png',
-'/assets/img/bitmoji_3.png',
-'/assets/img/bitmoji_2.png',
-'/assets/img/bitmoji_1.png',
-'/assets/img/logoMano.png',
-'/assets/img/loritz.jpg',
-'/assets/img/bts-SNIR.png',
-'/assets/img/bac-SIN.png',
-'/assets/img/fcc-long-dark.png',
-'/assets/img/flower.png',
-'/assets/manifest.json',
-'/assets/img/icon/16x16.ico',
-'/assets/img/icon/32x32.ico',
-'/assets/img/icon/48x48.ico',
-'/assets/img/icon/64x64.ico',
-'/assets/img/icon/128x128.ico',
-'https://i.imgur.com/m67XYDX.png'
+  '/assets/translation/en.js',
+  '/assets/translation/fr.js',
+  '/assets/webfonts/fa-brands-400.woff2',
+  '/assets/webfonts/fa-solid-900.woff2',
+  '/assets/doc/loritz_plaquette_snir.pdf',
+  '/assets/doc/loritz_plaquette_sin.pdf',
+  '/assets/img/back.png',
+  '/assets/img/heart.png',
+  '/assets/img/bitmoji_3.png',
+  '/assets/img/bitmoji_2.png',
+  '/assets/img/bitmoji_1.png',
+  '/assets/img/logoMano.png',
+  '/assets/img/loritz.jpg',
+  '/assets/img/bts-SNIR.png',
+  '/assets/img/bac-SIN.png',
+  '/assets/img/fcc-long-dark.png',
+  '/assets/img/flower.png',
+  '/assets/manifest.json',
+  '/assets/img/icon/16x16.ico',
+  '/assets/img/icon/32x32.ico',
+  '/assets/img/icon/48x48.ico',
+  '/assets/img/icon/64x64.ico',
+  '/assets/img/icon/128x128.ico',
+  'https://i.imgur.com/m67XYDX.png'
 ];
-const cacheVersion = 'theopierne-cache-v1.0.5';
+const cacheVersion = 'theopierne-cache-v1.0.6';
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(cacheVersion).then((cache) => {
-            return cache.addAll(addToCache);
-        }).catch((err) => {
-            console.error(err);
-        })
-    )
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(cacheVersion).then((cache) => {
+      return cache.addAll(addToCache);
+    }).catch((err) => {
+      console.error(err);
+    })
+  )
 });
 
 self.addEventListener('activate', evt => {
@@ -40,18 +42,18 @@ self.addEventListener('activate', evt => {
       return Promise.all(keys
         .filter(key => key !== cacheVersion)
         .map(key => caches.delete(key))
-        );
+      );
     })
-    );
+  );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.match(event.request).then(function(cacheRes) {
+    caches.match(event.request).then(function (cacheRes) {
       if (cacheRes) return cacheRes;
-      return fetch(event.request).then(function(res) {
+      return fetch(event.request).then(function (res) {
         return res;
-      }).catch(function(err) {
+      }).catch(function (err) {
         console.error(err);
       });
     }));
